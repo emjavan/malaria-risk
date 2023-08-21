c8d9a004df35ed03b27535734d6e1f27081881ee
Emily Javan <emjavan@gmail.com>
2023-08-21 10:51:28 -0500

    launcher and commands to run missing sims 1K x10

--- code/frontera_parallel_code/launch_malaria_missing.sh
+++ code/frontera_parallel_code/launch_malaria_missing.sh
@@ -5,7 +5,7 @@
 #SBATCH -e missing_r0_import.%j.e        # Name of stderr output file (%j expands to jobId)
 #SBATCH -p small                         # Queue name, small is for <=2 nodes, normal 3+
 #SBATCH -N 1                  	         # Total number of nodes requested
-#SBATCH -n 4                             # Total number of tasks to run 56 cores/node (28 per socket)
+#SBATCH -n 40                            # Total number of tasks to run 56 cores/node (28 per socket)
 #SBATCH -t 48:00:00            	         # Run time (hh:mm:ss)
 #SBATCH -A A-ib1                         # Allocation name
 #SBATCH --mail-user=emjavan@utexas.edu   # Email for notifications
